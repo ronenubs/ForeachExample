13355adf00dcee16a665c12186033e5382526527
Ron Enubs <ronenubs@gmail.com>
2020-11-04 10:55:13 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 45, 5, 7, 32, 64, 4 };
+
+            //for(int index = 0; index < nums.Length; index++)
+            //{
+            //    Console.Write(" {0}", nums[index]);
+            //}
+
+            DisplayNumbers(a);
+
+            Console.ReadLine();
+        }
+
+        private static void DisplayNumbers(int[] nums)
+        {
+            foreach (int temp in nums)
+            {
+                Console.Write(" {0}", temp);
+            }
+        }
+    }
+}
